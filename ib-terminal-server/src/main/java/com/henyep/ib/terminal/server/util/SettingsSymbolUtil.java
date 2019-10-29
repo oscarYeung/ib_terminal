4ba08a5804637e3fe156cfd0d26c25d47a42f42e
oscarYeung <oscar.yeung@gmail.com>
2019-10-29 12:20:15 +0800

    remove exchange rate update

--- ib-terminal-server/src/main/java/com/henyep/ib/terminal/server/util/SettingsSymbolUtil.java
+++ ib-terminal-server/src/main/java/com/henyep/ib/terminal/server/util/SettingsSymbolUtil.java
@@ -1,22 +1,17 @@
 package com.henyep.ib.terminal.server.util;
 
+import java.util.Date;
 import java.util.List;
 
-
 import com.henyep.ib.terminal.api.dto.db.SettingsSymbolBean;
-import com.henyep.ib.terminal.api.dto.db.SettingsUsdCurrencyExchangeBean;
 
 public class SettingsSymbolUtil {
-	
 
 	private List<SettingsSymbolBean> beanList;
 	
-	private List<SettingsUsdCurrencyExchangeBean> exchangeBeanList;
-	
-	public SettingsSymbolUtil(List<SettingsSymbolBean> beanList, List<SettingsUsdCurrencyExchangeBean> exchangeBeans){
+	public SettingsSymbolUtil(List<SettingsSymbolBean> beanList){
 		
 		this.beanList = beanList;
-		this.exchangeBeanList = exchangeBeans;
 	}
 	
 	public SettingsSymbolBean GetSettingBean(String symbol){
@@ -26,35 +21,6 @@ public class SettingsSymbolUtil {
 				return bean;
 			}
 		}
-		return null;
-	}
-	
-	
-	public Double GetExchangeRate(String baseCurrency){
-		
-		if(baseCurrency.equals("USD")){
-			return 1.0;
-		}
-		
-		for(SettingsUsdCurrencyExchangeBean exchangeBean : this.exchangeBeanList){
-			
-			if(exchangeBean.getBase_currency().equals(baseCurrency)){
-				String exchangeSymbol = exchangeBean.getExchange_symbol();
-				for(SettingsSymbolBean bean : beanList){
-					if(bean.getSymbol().equals(exchangeSymbol)){
-						if(exchangeBean.getIs_cross()){
-							System.out.println(baseCurrency + " 1/xxxxxxx" + 1 / bean.getClosing_rate());
-							return 1 / bean.getClosing_rate();
-						}
-						else{
-							System.out.println(baseCurrency + " xxxxxxx" + bean.getClosing_rate().toString());
-							return bean.getClosing_rate();
-						}
-					}
-				}
-			}
-		}
-		System.out.println("oh return null");
 		return null;
 		
 	}
